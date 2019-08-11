b5393ec3790319c7ff8db2ab149c9553ead60577
Zhi Li <zhili@s-149-142-48-50.resnet.ucla.edu>
2019-08-11 14:07:23 -0700

    loadweight

---
+++ loadenv.py
@@ -0,0 +1,14 @@
+agent.actor_local.load_state_dict(torch.load('checkpoint_actor.pth'))
+agent.critic_local.load_state_dict(torch.load('checkpoint_critic.pth'))
+
+state = env.reset()
+agent.reset()   
+while True:
+    action = agent.act(state)
+    env.render()
+    next_state, reward, done, _ = env.step(action)
+    state = next_state
+    if done:
+        break
+        
+env.close()
